e94c566f123623b43cc339755051fa602884cf06
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-14 16:08:19 +0530

    Updated and completed the integration of tailwind

--- 03_PropsWithTailwind/src/App.jsx
+++ 03_PropsWithTailwind/src/App.jsx
@@ -2,32 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const myArr = [12, 23, 34];
+  const myObj = {
+    Name: "Sandeep",
+    Age: 22,
+    Hobby: "Development and Singing"
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
+      <Card userName="Sandeep Wadhawan" src="https://avatars.githubusercontent.com/u/68498988?v=4" designation="Software Developer" location="(New Delhi, India)"/>
+      <Card userName="Hitesh Choudhary" src="https://avatars.githubusercontent.com/u/11613311?v=4" designation="Software Engineer" location=" (Bangalore, India)"/>
     </>
   )
 }
