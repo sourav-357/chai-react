96cc74569529f146d1382e925e11eac51286cd4f
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-14 09:58:12 +0530

    first code

--- 01_basic-react/src/App.js
+++ 01_basic-react/src/App.js
@@ -1,13 +1,24 @@
-// This App() function returns the HTML. So basically it is a functional component 
-
-import Chai from './Chai.js'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <h1>Starting With React | Sandeep Wadhawan</h1>
-      <Chai/>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- 01_basic-react/src/index.js
+++ 01_basic-react/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// React ek khud ka DOM bhi banata hai jisko hum virtual DOM bolte hain and compare it with real DOM ki kitna difference hai dono DOM's mein and jahan bhi elements mein changes required hote hain sirf wahan wahan changes karta hai
-const root = ReactDOM.createRoot(document.getElementById('root')); //ReactDOM ka ek method hai createRoot()
-
-// Jase HTML mein browser ka DOM render karta hai HTML ko wase yahan bhi root.render() likhte hain and render hum yahan App ko karte hain
-
-// So basically root.render() is used to render a React component into the DOM
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App /> 
-  // We can create custom tags(<App />) in react which is known as JSX (Javascript XML)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
