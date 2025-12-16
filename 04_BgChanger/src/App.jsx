98ede7f743a81ecbf7db45b975f2b9c3d343ab1f
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-16 15:50:38 +0530

    folder structure

--- 04_BgChanger/src/App.jsx
+++ 04_BgChanger/src/App.jsx
@@ -5,12 +5,15 @@ function App() {
   const [color, setColor] = useState("lightyellow"); // 'lightyellow' is the default color
 
   return (
+
     <div
       className="w-full h-screen duration-200"
       style={{ backgroundColor: color }}
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
+
           <button
             onClick={() => setColor("red")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
@@ -81,9 +84,13 @@ function App() {
           >
             Dark
           </button>
+          
         </div>
+
       </div>
+
     </div>
+
   );
 }
 
