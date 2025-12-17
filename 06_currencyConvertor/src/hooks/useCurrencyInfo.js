0a6256dfa544bb17b8be99967c417b588e7d20cb
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-17 15:40:45 +0530

    Completed the React part

--- 06_currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06_currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,25 +1,20 @@
-// required hook to create a new custom hook
-import { useState, useEffect } from "react";
+// Our custom hooks can use built-in hooks like useEffect(), useState()
 
-// creating our new custom hook which takes 'currency' as input parameter
-function useCurrencyInfo(currency) {
-  // setting up the data as per the resposnse of the 'API'
-  const [data, setData] = useState({});
+// Note : A custom Hook is a JavaScript function whose name starts with ”use” and that may call other Hooks.
 
-  // This 'API' needs to be called once the 'currency' is changed --> that is why we took 'currency' as input 'parameter'
-  // using the useEffect() hook as we need to call this 'API' on every change in 'currency' value
-  useEffect(() => {
-    fetch(
-      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
-    )
-      .then((res) => res.json()) // convert the json data to normal data
-      .then((res) => setData(res.currency)) // set the 'data' as per the respond from 'API' and we only want 'currency' part of 'res'
-      .then(console.log(data));
-  }, [currency]); // 'currency' is passed as the changed parameter
+import { useEffect, useState } from "react";
 
-  // return value for the function
-  console.log(data);
-  return data;
+const useCurrencyInfo = (currency) => {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(`https://latest.currency-api.pages.dev/v1/currencies/USD.json`)
+        .then(res => res.json())
+        .then(data => setData(data[currency]))
+    }, [currency]);
+    console.log(data);
+    return data;
 }
 
 export default useCurrencyInfo;
+
---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTodo';
+import Navbar from './components/Navbar';
+import Todos from './components/Todos';
+
+function App() {
+  
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App;
