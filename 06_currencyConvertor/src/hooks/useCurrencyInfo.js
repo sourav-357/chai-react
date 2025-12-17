8a0be6a216e20e09e3b2c8db38349946eb98cd8e
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-17 10:52:31 +0530

    made a custom hook

---
+++ 06_currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,24 @@
+// required hook to create a new custom hook
+import { useState, useEffect } from "react";
+
+// creating our new custom hook which takes 'currency' as input parameter
+function useCurrencyInfo(currency) {
+  // setting up the data as per the resposnse of the 'API'
+  const [data, setData] = useState({});
+
+  // This 'API' needs to be called once the 'currency' is changed --> that is why we took 'currency' as input 'parameter'
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json()) // convert the json data to normal data 
+      .then((res) => setData(res.currency)) // set the 'data' as per the respond from 'API' and we only want 'currency' part of 'res'
+      .then(console.log(data));
+  }, [currency]); // 'currency' is passed as the changed parameter
+
+  // return value for the function
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
