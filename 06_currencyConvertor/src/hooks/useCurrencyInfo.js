72eaa7598a2bbe699a185671806fb4c4aad9a1e7
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-17 12:24:33 +0530

    updated teh comments

--- 06_currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06_currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -7,11 +7,12 @@ function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
 
   // This 'API' needs to be called once the 'currency' is changed --> that is why we took 'currency' as input 'parameter'
+  // using the useEffect() hook as we need to call this 'API' on every change in 'currency' value
   useEffect(() => {
     fetch(
       `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
     )
-      .then((res) => res.json()) // convert the json data to normal data 
+      .then((res) => res.json()) // convert the json data to normal data
       .then((res) => setData(res.currency)) // set the 'data' as per the respond from 'API' and we only want 'currency' part of 'res'
       .then(console.log(data));
   }, [currency]); // 'currency' is passed as the changed parameter
